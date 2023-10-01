09a3762d4bd08c471816c75a4d592e191c9097ed
quentindamois <quentin.damois@efrei.net>
2023-10-01 16:28:18 -0400

    BookNav componment created

---
+++ lab-2/src/components/BookNav.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+const emit = defineEmits(['user-input'])
+const UserInput = ref('');
+function sendUserInput() {
+    emit('user-input',UserInput.value)
+}
+</script>
+<template>
+    <nav>
+        <h2>
+            Enter a book name :
+        </h2>
+        <input type='text' v-model="UserInput" @input="sendUserInput">
+        <p>
+            {{  UserInput }}
+        </p>
+    </nav>
+</template>
