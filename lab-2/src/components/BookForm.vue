8b0b218f536a9a13c052e680bb8bf81cb85cc185
quentindamois <quentin.damois@efrei.net>
2023-10-01 16:27:03 -0400

    BookForm componment created

---
+++ lab-2/src/components/BookForm.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+const emit = defineEmits(['response'])
+const BookInputed = ref({
+    Title: '',
+    Author: '',
+    Page: Number
+})
+const buttonMessage = defineProps({
+    labelButton: String
+})
+function sendInputedBook() {
+    console.log("click");
+    emit('response', BookInputed.value);
+}
+</script>
+<template>
+    <form method="post" onsubmit="return false">
+        <label>
+            Title
+        </label>
+        <input type="text" v-model="BookInputed.Title">
+        <label>
+            Author
+        </label>
+        <input type="text" v-model="BookInputed.Author">
+        <label>
+            Page count
+        </label>
+        <input type="number" v-model="BookInputed.Page">
+        <button @click="sendInputedBook">
+            {{ labelButton || 'click on the button' }}
+        </button>
+    </form>
+</template>
