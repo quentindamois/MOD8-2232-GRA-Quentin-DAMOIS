4b3a4e36eaa359a913c4ace77e7c15476df81994
quentindamois <quentin.damois@efrei.net>
2023-10-01 16:29:05 -0400

    BookPage view created

---
+++ lab-2/src/views/BookPage.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, computed } from 'vue';
+import BookForm from "../components/BookForm.vue";
+let id = ref(0);
+const BookArray = ref([
+  {Id: ++id.value, Title: "Necronomicon", Author: "Lovecraft",  Page: 149},
+  {Id: ++id.value, Title: "King Lear", Author: "Sheakspear", Page: 80}
+])
+</script>
+<template>
+    <h1>
+        {{ BookArray[this.$route.params.id].Title }}
+    </h1>
+    <ul>
+        <li>
+            {{ BookArray[this.$route.params.id].Author }}
+        </li>
+        <li>
+            {{ BookArray[this.$route.params.id].Page }}
+        </li>
+    </ul>
+    <BookForm :label-button="'Save'" @response="(bookEntered) => {
+        BookArray[this.$route.params.id].Title = bookEntered.Title;
+        BookArray[this.$route.params.id].Author = bookEntered.Author;
+        BookArray[this.$route.params.id].Page = bookEntered.Author
+    }"/>
+</template>
